b89222e4e78b4687f79673ceb7d63e2388a07f7a
DEA_CMOURA <dea_cmoura@fastalelo.onmicrosoft.com>
2022-10-05 09:57:14 -0300

    init

---
+++ api-senso-start/api-senso-start/src/main/java/com/deal/apisensostart/ApiSensoStartApplication.java
@@ -0,0 +1,13 @@
+package com.deal.apisensostart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSensoStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSensoStartApplication.class, args);
+	}
+
+}
---
+++ api-senso-start/api-senso-start/src/test/java/com/deal/apisensostart/ApiSensoStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deal.apisensostart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSensoStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
